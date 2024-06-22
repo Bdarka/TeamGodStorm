0c9cbe78faa96ca1a3f2410446fbda83f481cc86
Bdarka <kslackie@inside.artcenter.edu>
2024-06-22 13:19:56 -0700

    Created GameManager and MusicManager Objects
    Starting blueprints for basic game management systems

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> objectsToDestroy = new List<GameObject>();
+    public List<GameObject> objectsToSave = new List<GameObject>();
+
+    public Transform destroyParent;
+    public Transform saveParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       foreach(Transform t in destroyParent)
+        {
+            objectsToDestroy.Add(t.gameObject);
+        }
+
+       foreach(Transform t in saveParent)
+        {
+            objectsToSave.Add(t.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
