38a8753e9a4d6a26342204ca608e29b815b9b1b2
Bdarka <kslackie@inside.artcenter.edu>
2024-06-24 13:34:01 -0700

    Added Options Menu Fuctions
    Players can now adjust various settings like resolution, full screen, graphics quality, and volume

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject optionsMenu;
 
     public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    public void Start()
+    {
+        ResolutionSetup();
+    }
 
     public void OpenMenu()
     {
@@ -27,4 +37,49 @@ public class OptionsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("Volume", volume);
     }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void ResolutionSetup()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> resolutionList = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            // string option = "width" + "x" + "height";
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            resolutionList.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resolutionList);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+
 }
