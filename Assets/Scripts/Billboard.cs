2809c92a35d941a4b02ca5f0b4914cd5c09cd946
Taf Islam <152765931+Taffy0321@users.noreply.github.com>
2024-07-11 10:52:57 -0400

    Added Billboarding for sprite following.
    Included billboarding script in order to make sure sprites follow the camera.

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    void Start()
+    {
+        // Get the main camera in the scene
+        mainCamera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        // Make the sprite face the camera
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
+                         mainCamera.transform.rotation * Vector3.up);
+    }
+}
